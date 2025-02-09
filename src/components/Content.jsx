604d794c51cef9bfb58ee049e5c19cf6979a8667
Nitish Kumar <iamnitishsah12@gmail.com>
2025-02-10 02:13:52 +0530

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -11,45 +11,51 @@ import UpcomingSchedule from '../assets/Upcoming Schedule.svg';
 
 const Content = () => {
     return (
-        <div className="flex flex-col ml-4">
-            <div className="Title">
-                <img src={Title} alt="Title"/>
+        <div className="flex flex-col px-4 sm:px-6 lg:px-8 max-w-[1600px] mx-auto">
+            {/* Title Section */}
+            <div className="mb-6 flex justify-center md:justify-start">
+                <img src={Title} alt="Title" className="w-40 md:w-52" />
             </div>
 
-            <div className="flex flex-row">
-                <div className="LeftContent">
-                    <div className="flex flex-row justify-center">
-                        <div className="AvailablePosition">
-                            <img src={AvailablePosition} alt="AvailablePosition"/>
+            <div className="flex flex-col lg:flex-row gap-6">
+                {/* Left Content */}
+                <div className="flex flex-col gap-4 w-full lg:w-2/3">
+                    {/* Top Metrics */}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                        <div className="bg-white p-4 rounded-xl shadow flex justify-center">
+                            <img src={AvailablePosition} alt="Available Position" className="w-full max-w-[200px]" />
                         </div>
-                        <div className="JobOpen">
-                            <img src={JobOpen} alt="JobOpen"/>
+                        <div className="bg-white p-4 rounded-xl shadow flex justify-center">
+                            <img src={JobOpen} alt="Job Open" className="w-full max-w-[200px]" />
                         </div>
-                        <div className="NewEmployee">
-                            <img src={NewEmployee} alt="NewEmployee"/>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-row">
-                        <div className="TotalEmployees">
-                            <img src={TotalEmployees} alt="TotalEmployees"/>
-                        </div>
-                        <div className="TalentRequests">
-                            <img src={TalentRequests} alt="TalentRequests"/>
+                        <div className="bg-white p-4 rounded-xl shadow flex justify-center">
+                            <img src={NewEmployee} alt="New Employee" className="w-full max-w-[200px]" />
                         </div>
                     </div>
 
-                    <div className="Announcement">
-                        <img src={Announcement} alt="Announcement"/>
+                    {/* Employee & Talent Section */}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                        <div className="bg-white p-4 rounded-xl shadow flex justify-center">
+                            <img src={TotalEmployees} alt="Total Employees" className="w-full max-w-[250px]" />
+                        </div>
+                        <div className="bg-white p-4 rounded-xl shadow flex justify-center">
+                            <img src={TalentRequests} alt="Talent Requests" className="w-full max-w-[250px]" />
+                        </div>
+                    </div>
+
+                    {/* Announcements */}
+                    <div className="bg-white p-4 rounded-xl shadow flex justify-center">
+                        <img src={Announcement} alt="Announcement" className="w-full max-w-[300px]" />
                     </div>
                 </div>
 
-                <div className="RightContent">
-                    <div>
-                        <img src={RecentActivity} alt="RecentActivity"/>
+                {/* Right Content */}
+                <div className="flex flex-col gap-4 w-full lg:w-1/3">
+                    <div className="bg-white p-4 rounded-xl shadow flex justify-center">
+                        <img src={RecentActivity} alt="Recent Activity" className="w-full max-w-[300px]" />
                     </div>
-                    <div>
-                        <img src={UpcomingSchedule} alt="UpcomingSchedule"/>
+                    <div className="bg-white p-4 rounded-xl shadow flex justify-center">
+                        <img src={UpcomingSchedule} alt="Upcoming Schedule" className="w-full max-w-[300px]" />
                     </div>
                 </div>
             </div>
